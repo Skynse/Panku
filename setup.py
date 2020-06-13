d25835e6401a178c595cef42e5b9bfe2bd5c2a68
Skynse <austinamakyeansah@gmail.com>
2020-06-13 22:18:29 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setuptools.setup(
     author='Skynse',
     long_description=long_description,
     #package_data={'': ['./requirements.txt']},
-    packages=setuptools.find_packages()
+    packages=setuptools.find_packages(),
     include_package_data=True,
     author_email='',
     description='Easy API-less reddit scraper',
