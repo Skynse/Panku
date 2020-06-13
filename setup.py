d0c207c0b39e3f47506c1b8cdc0eaed06bd8245e
Skynse <austinamakyeansah@gmail.com>
2020-06-13 22:37:58 +0000

    Finalized setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ with open("README.md",'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='Panku-Skynse',
+    name='panku',
     scripts=['Panku/panku'],
     version='0.1',
     license='MIT',
     author='Skynse',
+    long_description_content_type='text/markdown',
     long_description=long_description,
     #package_data={'': ['./requirements.txt']},
     packages=setuptools.find_packages(),
