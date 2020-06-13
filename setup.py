56ceb7a708209180e9cb6fe38ff88c2df33bfb97
Skynse <austinamakyeansah@gmail.com>
2020-06-13 22:17:01 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from distutils.core import setup
+import setuptools
 with open("README.md",'r') as fh:
     long_description = fh.read()
 
-setup(
+setuptools.setup(
     name='Panku-Skynse',
     scripts=['Panku/panku'],
     version='0.1',
@@ -10,7 +10,8 @@ setup(
     license='MIT',
     author='Skynse',
     long_description=long_description,
-    package_data={'': ['./requirements.txt']},
+    #package_data={'': ['./requirements.txt']},
+    packages=setuptools.find_packages(),
     include_package_data=True,
     author_email='',
     description='Easy API-less reddit scraper',
